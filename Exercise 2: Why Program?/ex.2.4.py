53adfbf555ac7c123db06592f9ba9b8f7b15098f
l-fowler <77890312+l-fowler@users.noreply.github.com>
2021-07-22 17:27:11 +0100

    Update and rename ex.2.4.py to Exercise 2: Why Program?/ex.2.4.py

---
+++ Exercise 2: Why Program?/ex.2.4.py
@@ -0,0 +1,29 @@
+"""
+Exercise  4: Assume that we execute the following assignment statements:
+
+width = 17
+height = 12.0
+
+For each of the following expressions, write the value of the expression 
+and the type (of the value of the expression).
+"""
+
+# 1. width//2
+a = width//2
+print(a)
+print(type(a))
+
+# 2. width/2.0
+b = width/2.0
+print(b)
+print(type(b))
+
+# 3. height/3
+c = height/3
+print(c)
+print(type(c))
+
+# 4. 1 + 2 * 5
+d = 1 + 2 * 5
+print(d)
+print(type(d))
