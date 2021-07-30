0a29d5b8533d82eda8c6e3709ee374b71416e14e
Lorna <lorna.fowler6@gmail.com>
2021-07-30 17:46:28 +0100

    add ex.7.3.py

---
+++ Chapter 7: Files/ex.7.3.py
@@ -0,0 +1,46 @@
+"""
+Exercise 3: Sometimes when programmers get bored or want to have a bit of fun, they
+add a harmless Easter Egg to their program. Modify the program that prompts the user
+for the file name so that it prints a funny message when the user types in the exact
+file name “na na boo boo”. The program should behave normally for all other files which
+exist and don’t exist. Here is a sample execution of the program:
+
+python egg.py
+Enter the file name: mbox.txt
+There were 1797 subject lines in mbox.txt
+
+python egg.py
+Enter the file name: missing.tyxt
+File cannot be opened: missing.tyxt
+
+python egg.py
+Enter the file name: na na boo boo
+NA NA BOO BOO TO YOU - You have been punk'd!
+We are not encouraging you to put Easter Eggs in your programs; this is just an exercise.
+
+You can download the files from:
+www.py4e.com/code3/mbox-short.txt and www.py4e.com/code3/mbox.txt
+"""
+
+# Input from user
+fname = input("Enter file name: ")
+# Try/Except
+try:
+    # If/Else
+    if fname == 'na na boo boo':
+        print("NA NA BOO BOO TO YOU - You have been punk'd!")
+        quit()
+    else:
+        fhand = open(fname)
+# Except FileNotFoundError
+except FileNotFoundError:
+    print("File cannot be opened: " + fname)
+    quit()
+
+# Counter variable = 0
+count = 0
+# For loop - line.startswith, count + 1
+for line in fhand:
+    if line.startswith('Subject:'):
+        count = count + 1
+print('There were', count, 'subject lines in', fname)
