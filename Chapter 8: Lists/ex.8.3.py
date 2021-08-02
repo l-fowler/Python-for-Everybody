4eaa7411a99632c62ea13869f6cf3a0dca58fd8c
Lorna <lorna.fowler6@gmail.com>
2021-08-02 22:16:57 +0100

    add ex.8.3.py

---
+++ Chapter 8: Lists/ex.8.3.py
@@ -0,0 +1,16 @@
+"""
+Exercise 3: Rewrite the guardian code in the above example without two if statements.
+Instead, use a compound logical expression using the or logical operator with a single
+if statement.
+
+You can download the files from:
+www.py4e.com/code3/mbox-short.txt
+"""
+
+fhand = open('mbox-short.txt')
+count = 0
+for line in fhand:
+    words = line.split()
+    # print('Debug:', words)
+    if len(words) == 0 or words[0] != 'From' : continue
+    print(words[2])
